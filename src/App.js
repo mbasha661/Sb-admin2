b1bd0db52421b2f6b7e990172e476111b35bedce
mbasha661 <mbasha661@gmail.com>
2024-02-12 09:08:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Sidebar from "./components/Sidebar";
+import Dashbord from "./components/Dashbord";
+
+
+function App() {
+  return <>
+    <div id="wrapper">
+    <Sidebar/>
+    <Dashbord/>
+    </div>
+
+   
+
+
+  </>
+}
+
+export default App;
